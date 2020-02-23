1d553d10b15c126fd602edc29df14ca20b86ecb7
docker <docker@example.com>
2020-02-23 15:37:41 +0200

    wip: model implements interface

--- app/code/Stanislavz/Chat/Api/Data/MessageInterface.php
+++ app/code/Stanislavz/Chat/Api/Data/MessageInterface.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace Stanislavz\Chat\Api\Data;
 
+use Magento\Tests\NamingConvention\true\string;
+
 interface MessageInterface
 {
     /**
      * @return int|string|null
      */
-    public function getMessageId();
+    public function getId();
 
     /**
      * @param int|string $messageId
-     * @return $this
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
      */
-    public function setMessageId($messageId): self;
+    public function setId($messageId): \Stanislavz\Chat\Api\Data\MessageInterface;
 
     /**
      * @return int|string|null
@@ -24,7 +26,84 @@ interface MessageInterface
 
     /**
      * @param int|string $messageId
-     * @return $this
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
      */
-    public function setMessageId($messageId): self;
+    public function setMessageId($messageId): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return string|null
+     */
+    public function getAuthorType(): ?string;
+
+    /**
+     * @param string $authorType
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setAuthorType($authorType): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return int|string|null
+     */
+    public function getAuthorId();
+
+    /**
+     * @param int|string $authorId
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setAuthorId($authorId): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return string|null
+     */
+    public function getAuthorName(): ?string;
+
+    /**
+     * @param string $authorName
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setAuthorName($authorName): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string;
+
+    /**
+     * @param string $message
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setMessage($message): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return int|string|null
+     */
+    public function getWebsiteId();
+
+    /**
+     * @param int|string $websiteId
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setWebsiteId($websiteId): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return string|null
+     */
+    public function getChatHash(): ?string;
+
+    /**
+     * @param string $chatHash
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setChatHash($chatHash): \Stanislavz\Chat\Api\Data\MessageInterface;
+
+    /**
+     * @return string|null
+     */
+    public function getCreatedAt(): ?string;
+
+    /**
+     * @param string $createdAt
+     * @return \Stanislavz\Chat\Api\Data\MessageInterface
+     */
+    public function setCreatedAt($createdAt): \Stanislavz\Chat\Api\Data\MessageInterface;
 }
--- app/code/Stanislavz/Chat/Model/Message.php
+++ app/code/Stanislavz/Chat/Model/Message.php
@@ -4,7 +4,185 @@ declare(strict_types=1);
 
 namespace Stanislavz\Chat\Model;
 
-class Message
-{
+use Stanislavz\Chat\Api\Data\MessageInterface;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
 
+class Message extends \Magento\Framework\Model\AbstractModel implements MessageInterface
+{
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\Registry $registry,
+        AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _construct()
+    {
+        $this->_init(\Stanislavz\Chat\Model\ResourceModel\Message::class);
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getId()
+    {
+        return $this->getData('message_id');
+    }
+
+    /**
+     * @param int|string $messageId
+     * @return MessageInterface
+     */
+    public function setId($messageId): MessageInterface
+    {
+        return $this->setData('message_id', $messageId);
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getMessageId()
+    {
+        return $this->getData('message_id');
+    }
+
+    /**
+     * @param int|string $messageId
+     * @return MessageInterface
+     */
+    public function setMessageId($messageId): MessageInterface
+    {
+        return $this->getData('message_id');
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getAuthorId()
+    {
+        return $this->getData('author_id');
+    }
+
+    /**
+     * @param int|string $authorId
+     * @return MessageInterface
+     */
+    public function setAuthorId($authorId): MessageInterface
+    {
+        return $this->setData('author_id', $authorId);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthorType(): ?string
+    {
+        return $this->getData('author_type');
+    }
+
+    /**
+     * @param string $authorType
+     * @return MessageInterface
+     */
+    public function setAuthorType($authorType): MessageInterface
+    {
+        return $this->setData('author_type', $authorType);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthorName(): ?string
+    {
+        return $this->getData('author_name');
+    }
+
+    /**
+     * @param string $authorName
+     * @return MessageInterface
+     */
+    public function setAuthorName($authorName): MessageInterface
+    {
+        return $this->setData('author_name', $authorName);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->getData('author_name');
+    }
+
+    /**
+     * @param string $message
+     * @return MessageInterface
+     */
+    public function setMessage($message): MessageInterface
+    {
+        return $this->setData('message', $message);
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getWebsiteId()
+    {
+        return $this->getData('website_id');
+    }
+
+    /**
+     * @param int|string $websiteId
+     * @return MessageInterface
+     */
+    public function setWebsiteId($websiteId): MessageInterface
+    {
+        return $this->setData('website_id', $websiteId);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getChatHash(): ?string
+    {
+        return $this->getData('chat_hash');
+    }
+
+    /**
+     * @param string $chatHash
+     * @return MessageInterface
+     */
+    public function setChatHash($chatHash): MessageInterface
+    {
+        return $this->setData('chat_hash', $chatHash);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCreatedAt(): ?string
+    {
+        return $this->getData('created_at');
+    }
+
+    /**
+     * @param string $createdAt
+     * @return MessageInterface
+     */
+    public function setCreatedAt($createdAt): MessageInterface
+    {
+        return $this->setData('created_at', $createdAt);
+    }
 }
